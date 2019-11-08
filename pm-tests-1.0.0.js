75c6de8f28d96c99590a11bb07ba6a22a881c16d
jptigersolv <48766360+jptigersolv@users.noreply.github.com>
2019-11-08 13:31:58 +0000

    Update pm-tests-1.0.0.js

--- pm-tests-1.0.0.js
+++ pm-tests-1.0.0.js
@@ -1,14 +1,3 @@
-//pm.environment.set("buildUrl",function (){
-//    var url = pm.environment.get("PROTOCOL") + '://' + pm.environment.get("IPADDRESS");
-//    if(pm.environment.get("PORT")!='443' && pm.environment.get("PORT")!='80')
-//    {
-//       url = url + ':' + pm.environment.get("PORT");
-//    }
-//    url = url + '/api/' + pm.environment.get("ControllerName");
-//    return url;
-//});
-
-
 function testResponseLength() {
     console.log("testResponseLength - Starting...");
     pm.test("Response length", function() {
